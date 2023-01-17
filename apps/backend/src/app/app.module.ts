8a1bf5df172d1bb5891289327f8f8fff49283ace
Rafał Rębilas <rrafal7@gmail.com>
2023-01-18 00:19:43 +0100

    Working local connection with mogno

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/meetme')],
   controllers: [AppController],
   providers: [AppService],
 })
