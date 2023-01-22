c2a86f1b7bbd1333ff9ee846ec62ffd91df1ec8f
Rafał Rębilas <rrafal7@gmail.com>
2023-01-22 20:19:11 +0100

    Clear default components

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose'
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017/meetme')],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
