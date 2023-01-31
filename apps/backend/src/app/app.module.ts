692730ef65eaf98fc9afd350e0fc7c03d17c2376
Rafał Rębilas <rrafal7@gmail.com>
2023-01-31 22:14:07 +0100

    Create core lib for configuration

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ApiCoreFeatureModule } from '@meet-me/api/core/feature';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/meetme')],
+  imports: [ApiCoreFeatureModule],
   controllers: [AppController],
 })
 export class AppModule {}
