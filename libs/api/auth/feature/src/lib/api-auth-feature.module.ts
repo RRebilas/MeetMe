a4a1e17452993c5e2a903a2e1c09b6ad797ecea6
Rafał Rębilas <rrafal7@gmail.com>
2023-02-20 20:27:54 +0100

    Replace explicit jwt secret with one from env variables

--- libs/api/auth/feature/src/lib/api-auth-feature.module.ts
+++ libs/api/auth/feature/src/lib/api-auth-feature.module.ts
@@ -2,14 +2,13 @@ import { AuthService } from '@meet-me/api/auth/data-access';
 import { Module } from '@nestjs/common';
 import { JwtStrategy, LocalStrategy } from './strategies';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { ApiUsersFeatureModule } from '@meet-me/api/users/feature';
 
 @Module({
   imports: [
     ApiUsersFeatureModule,
     JwtModule.register({
-      secret: jwtConstants.secret,
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1h' },
     }),
   ],
--- libs/api/auth/feature/src/lib/strategies/jwt.strategy.ts
+++ libs/api/auth/feature/src/lib/strategies/jwt.strategy.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { jwtConstants } from '../constants';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly _configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
+      secretOrKey: _configService.get('JWT_SECRET'),
     });
   }
 
