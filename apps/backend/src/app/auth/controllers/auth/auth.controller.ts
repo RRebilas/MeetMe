c7dbc6961f12368efee6853076d61f4dae6af881
Rafał Rębilas <rrafal7@gmail.com>
2023-03-02 18:53:32 +0100

    Create LocalAuthGuard

--- apps/backend/src/app/auth/controllers/auth/auth.controller.ts
+++ apps/backend/src/app/auth/controllers/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { CreateUserDto, LoginUserDto } from '@meet-me/shared/data-models';
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from '../../../../common/guards/local-auth.guards';
 import { User } from '../../../../models/User/schemas/user.schema';
 import { UsersService } from '../../../users/services/users.service';
 
@@ -14,7 +14,7 @@ export class AuthController {
   }
 
   @Post('login')
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   async login(@Body() loginUserDto: LoginUserDto): Promise<string> {
     return 'hello here ' + loginUserDto.username;
   }
